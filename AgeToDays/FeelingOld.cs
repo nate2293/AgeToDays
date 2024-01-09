991abf12c0340968752fccf45c88d89837828e7f
nate2293 <Mc_Garvey-N@ulster.ac.uk>
2024-01-09 21:55:02 +0000

    Add project files.

---
+++ AgeToDays/FeelingOld.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace AgeToDays
+{
+    internal class FeelingOld
+    {
+        public static int CalcAge(int age)      // Always ensure capital letters in method heading
+        {
+            int days = age * 365;     // Conversion of age into days
+            return days;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(CalcAge(42));     // 42 x 365 = 15,330
+            Console.WriteLine();
+
+            Console.WriteLine(CalcAge(10));     // 10 x 365 = 3,650
+            Console.WriteLine();
+
+            Console.WriteLine(CalcAge(25));     // 25 x 365 = 9,125
+            Console.WriteLine();
+        }
+    }
+}
